bfad9579ec8cbc612e551b2bdbe6cc8379739c0c
Deepanjali Tripathi <deepanjalitripathi7@gmail.com>
2022-07-25 20:04:55 +0530

    initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    // debugShowCheckedModeBanner: false,
+    title: 'Awesome Apps',
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Awesome App'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Align(
+            // alignment: Alignment
+            child: Container(
+              color: Colors.black,
+              width: 200,
+              height: 400,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    width: 100,
+                    height: 100,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(9),
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    width: 100,
+                    height: 100,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    // debugShowCheckedModeBanner: false,
+    title: 'Awesome Apps',
+    home: HomePage(),
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('Awesome App'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Align(
+            // alignment: Alignment
+            child: Container(
+              color: Colors.black,
+              width: 200,
+              height: 400,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    width: 100,
+                    height: 100,
+                    color: Colors.red,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(9),
+                    width: 100,
+                    height: 100,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    padding: const EdgeInsets.all(8),
+                    width: 100,
+                    height: 100,
+                    color: Colors.green,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ));
+  }
+}
