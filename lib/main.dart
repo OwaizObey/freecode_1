cf29477bff850511dd76353b3f4a9f78fc957f34
Deepanjali Tripathi <deepanjalitripathi7@gmail.com>
2022-07-25 20:09:20 +0530

    c

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    // debugShowCheckedModeBanner: false,
+    debugShowCheckedModeBanner: false,
     title: 'Awesome Apps',
     home: HomePage(),
   ));
@@ -14,43 +14,44 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Awesome App'),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Align(
-            // alignment: Alignment
-            child: Container(
-              color: Colors.black,
-              width: 200,
-              height: 400,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    width: 100,
-                    height: 100,
-                    color: Colors.red,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(9),
-                    width: 100,
-                    height: 100,
-                    color: Colors.yellow,
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    width: 100,
-                    height: 100,
-                    color: Colors.blue,
-                  ),
-                ],
-              ),
+      appBar: AppBar(
+        title: Text('Awesome App'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Align(
+          // alignment: Alignment
+          child: Container(
+            color: Colors.black,
+            width: 200,
+            height: 400,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  width: 100,
+                  height: 100,
+                  color: Colors.red,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(9),
+                  width: 100,
+                  height: 100,
+                  color: Colors.yellow,
+                ),
+                Container(
+                  padding: const EdgeInsets.all(8),
+                  width: 100,
+                  height: 100,
+                  color: Colors.blue,
+                ),
+              ],
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
