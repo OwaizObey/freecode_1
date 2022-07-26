e725aa71a46753348f141166b51565a0ecf55619
Deepanjali Tripathi <deepanjalitripathi7@gmail.com>
2022-07-26 14:23:01 +0530

    CLI

--- lib/main.dart
+++ lib/main.dart
@@ -42,7 +42,7 @@ class HomePage extends StatelessWidget {
             ListTile(
               leading: Icon(Icons.email),
               title: Text('Email'),
-              subtitle: Text('OwaizObey12@gmail.com'),
+              subtitle: Text('OwaizObey12@gmails.com'),
               trailing: Icon(Icons.send),
             ),
           ],
